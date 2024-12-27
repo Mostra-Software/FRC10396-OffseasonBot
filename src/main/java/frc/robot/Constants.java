86eda32b6dcfc94318ebdb286350e2a761e15d32
Tamer Guven <tamerguven07@hotmail.com>
2024-12-27 18:27:14 +0300

    First Day Changes

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -72,7 +72,7 @@ public final class Constants {
   public static class IntakeConstants {
 
     public static final int motorID = 0;
-    public static final int sensorID = 0;
+    public static final int sensorID = 9;
     public static final NeutralMode neutralMode = NeutralMode.Brake;
     public static final boolean reversed = true;
 
